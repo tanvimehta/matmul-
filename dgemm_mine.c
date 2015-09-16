c1f016de4cc6e84dae8598628add2775769f1a04
Tanvi Mehta <tanvimmehta@gmail.com>
2015-09-15 21:12:07 -0400

    First attempt

--- dgemm_mine.c
+++ dgemm_mine.c
@@ -1,14 +1,17 @@
 const char* dgemm_desc = "My awesome dgemm.";
 
-void square_dgemm(const int M, const double *A, const double *B, double *C)
+void square_dgemm(const int M, const double A, const double B, double *C)
 {
     int i, j, k;
     for (i = 0; i < M; ++i) {
         for (j = 0; j < M; ++j) {
-            double cij = C[j*M+i];
-            for (k = 0; k < M; ++k)
-                cij += A[k*M+i] * B[j*M+k];
-            C[j*M+i] = cij;
+        	jm += M;
+            double cij = C[jm+i];
+            for (k = 0; k < M; ++k) {
+            	km += M; 
+                cij += A[km+i] * B[jm+k];
+            }
+            C[jm+i] = cij;
         }
     }
 }
